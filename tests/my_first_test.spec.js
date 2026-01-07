3b562d366c271649e45d87bbdd38224ebb40fd87
Nate Stoerzinger <nstoerz@gmail.com>
2026-01-06 20:19:11 -0600

    Adding new test cases for general site launch and verification

---
+++ tests/my_first_test.spec.js
@@ -0,0 +1,19 @@
+const {test, expect} = require('@playwright/test')
+
+test('My first test', async ({page}) => {
+        await page.goto('https://google.com')
+        await expect(page).toHaveTitle('Google')
+    });
+
+
+
+test('SauceDemo login and logout', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+});
