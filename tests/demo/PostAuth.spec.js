46aded76987b780e49b323d12857fe0071a86322
Nate Stoerzinger <nstoerz@gmail.com>
2026-01-14 12:27:30 -0600

    Switched sites back to saucedemo because that other one is an ad filled hell hole

--- tests/demo/PostAuth.spec.js
+++ tests/demo/PostAuth.spec.js
@@ -1,5 +1,5 @@
 import { test } from '@playwright/test';
 
 test('PostAuth test', async ({ page }) => {
-    await page.goto('https://practice.expandtesting.com/login');
+    await page.goto('https://www.saucedemo.com//inventory.html');
 });
--- tests/demo/auth.setup.ts
+++ tests/demo/auth.setup.ts
@@ -5,17 +5,17 @@ const authFile = path.join(__dirname, '..', '..', 'playwright', '.auth', 'user.j
 
 setup('authenticate', async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
-  await page.goto('https://practice.expandtesting.com/login');
-  await page.locator('[id="username"]').click();
-  await page.locator('[id="username"]').fill(process.env.LOGIN);
-  await page.locator('[id="password"]').click();
-  await page.locator('[id="password"]').fill(process.env.PASSWORD);
-  await page.locator('[id="submit-login"]').click();
+  await page.goto('https://www.saucedemo.com');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill(process.env.LOGIN);
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill(process.env.PASSWORD);
+  await page.locator('[data-test="login-button"]').click();
   // Wait until the page receives the cookies.
   //
-  // Sometimes login flow sets cookies in the process of several redirects.
+  // Sometimes login flow sets cookies in the process of several redirectsq.
   // Wait for the final URL to ensure that the cookies are actually set.
-  await page.waitForURL('**/secure');
+  await page.waitForURL('**/inventory.html');
   // Alternatively, you can wait until the page reaches a state where all cookies are set.
   // await expect(page.getByRole('title', { name: 'Products' })).toBeVisible();
 
