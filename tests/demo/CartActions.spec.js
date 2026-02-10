425b86403bd71d3e86eaac9388a22f7d013d7109
Nate Stoerzinger <nstoerz@gmail.com>
2026-02-10 13:27:08 -0600

    Added test for removing all items from cart

--- tests/demo/CartActions.spec.js
+++ tests/demo/CartActions.spec.js
@@ -27,3 +27,19 @@ test('Add all item to cart', async ({ page }) => {
     await page.locator('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click();
     await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('6');
 });
+
+test('Remove all item from cart', async ({ page }) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click();
+    await page.locator('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click();
+    await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+    await page.locator('[data-test="remove-sauce-labs-bolt-t-shirt"]').click();
+    await page.locator('[data-test="remove-sauce-labs-fleece-jacket"]').click();
+    await page.locator('[data-test="remove-sauce-labs-onesie"]').click();
+    await page.locator('[data-test="remove-test.allthethings()-t-shirt-(red)"]').click();
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).not.toBeVisible();
+});
